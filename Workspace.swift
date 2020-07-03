01df30af223fabb6faa1eb46f481293f21cb20ac
m4riusz <mariusz.sut.94+github@gmail.com>
2020-07-03 21:23:02 +0200

    added basic project structure

--- Workspace.swift
+++ Workspace.swift
@@ -2,7 +2,7 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let workspace = Workspace(name: "BingoApp", projects: [
-    "Projects/Core",
-    "Projects/Modules",
-    "Projects/BingoApp"
+    "Core",
+    "Modules",
+    "BingoApp"
 ])
